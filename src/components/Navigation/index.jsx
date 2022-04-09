fd85401854f975ccc339cb4b265fad349fd4d30a
JulianaKomarytska <juliana.komarytska@gmail.com>
2022-04-09 20:02:18 +0300

    rename navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -14,7 +14,7 @@ const Navigation = () => {
   return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">My Service</Link>
+          <Link className="navbar-brand" to="/">Groups</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                   data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                   aria-label="Toggle navigation">
